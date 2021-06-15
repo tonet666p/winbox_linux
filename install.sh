5f2561fed8f1da3f8876a4520851a019cf21ca53
Tonet Jallo <tonet666p@gmail.com>
2021-06-14 19:08:21 -0500

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://mt.lv/winbox64
+mkdir -p ~/.wine/drive_c/winbox/
+mv winbox.exe ~/.wine/drive_c/winbox/
+cp winbox.desktop ~/.local/share/applications/
+cp winbox.svg ~/.local/share/icons/
